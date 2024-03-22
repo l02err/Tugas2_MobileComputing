2f913e21f17499a142d763490e63604b73ca903a
l02err <extrajoss369@gmail.com>
2024-03-23 02:06:12 +0900

    Update Tugas2

--- app/src/main/java/com/example/myapplication/LogInAct.kt
+++ app/src/main/java/com/example/myapplication/LogInAct.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 //Halaman LogIn Aplikasi
@@ -16,6 +17,13 @@ class LogInAct : AppCompatActivity() {
         tvHaventAccount.setOnClickListener{
             val intenregi = Intent(this@LogInAct, RegistAct :: class.java)
             startActivity(intenregi)
+        }
+
+        val btnLogin1 = findViewById<Button>(R.id.btn_login1)
+        btnLogin1.setOnClickListener{
+            val intenlogin1 = Intent(this@LogInAct, HomeAct :: class.java)
+            startActivity(intenlogin1)
+            finish()
         }
 
         val tvLupaPw = findViewById<TextView>(R.id.tv_lupa_pw)
